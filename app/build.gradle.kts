206d1619dd5fc81f416b09e696d305a60a0bff43
PhoenixHear7 <cristian.vb01@hotmail.com>
2024-04-04 20:36:14 -0500

    Se modifico la pantalla de Home y se añadio una lista y algunos estilos.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.stepbystep"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.stepbystep"
         minSdk = 29
---
+++ app/src/main/java/com/example/stepbystep/DailyFoodsAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.stepbystep
+
+class DailyFoodsAdapter {
+}
