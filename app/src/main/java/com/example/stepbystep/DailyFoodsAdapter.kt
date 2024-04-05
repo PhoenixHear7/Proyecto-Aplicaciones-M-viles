c6014bd4b19c1ab50be668a6b642dc4f74ef5726
Jonathan Guzman <alejandroguxman381@gmail.com>
2024-04-04 22:00:03 -0500

    Se agrego la ui de forgot password y se adaptador para la la lista de comidas diarias

--- app/src/main/java/com/example/stepbystep/DailyFoodsAdapter.kt
+++ app/src/main/java/com/example/stepbystep/DailyFoodsAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.stepbystep
 
-class DailyFoodsAdapter {
+import android.app.Activity
+import android.hardware.biometrics.BiometricManager.Strings
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class DailyFoodsAdapter(val context: Activity,val layout: Int, val data: ArrayList<String>)
+    : ArrayAdapter<String>(context,layout, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = context.layoutInflater.inflate(layout, null)
+        val name = view.findViewById<TextView>(R.id.foodName)
+        name.text= data[position]
+        val imageID = context.resources.getIdentifier(data[position].lowercase(), "drawable", context.packageName)
+        val img = view.findViewById<ImageView>(R.id.imagefood)
+        img.setImageResource(imageID)
+        return view
+    }
 }
--- app/src/main/java/com/example/stepbystep/MainActivity.kt
+++ app/src/main/java/com/example/stepbystep/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.stepbystep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stepbystep.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // utilizamos el binding para que al seleccionar al opcion sign nos envie a la actividad de esta
+         binding.singupButton.setOnClickListener {
+             val i = Intent(this, RegisterActivity::class.java)
+             startActivity(i)
+         }
+        // utilizamos el binding para que al seleccionar la opcion log in sign nos envie a la actividad de esta
+
+        binding.loginButton.setOnClickListener {
+            val i = Intent(this, HomeActivity::class.java)
+            startActivity(i)
+        }
+
+        binding.forgotPassword.setOnClickListener {
+            val i = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(i)
+        }
     }
+
+
 }
