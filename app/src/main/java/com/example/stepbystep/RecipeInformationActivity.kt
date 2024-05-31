efda3daeb92364ff3450aff088b6e9dae242960a
Jonathan Guzman <alejandroguxman381@gmail.com>
2024-05-31 12:17:10 -0500

    Se cambio el logo de la app y se arreglo la ui donde se muetrasn los ingredientes de cada receta

--- app/src/main/java/com/example/stepbystep/RecipeInformationActivity.kt
+++ app/src/main/java/com/example/stepbystep/RecipeInformationActivity.kt
@@ -44,6 +44,10 @@ class RecipeInformationActivity : AppCompatActivity() {
         viewModel.isLoading.observe(this) { isLoading ->
             binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
         }
+
+        viewModel.ingredients.observe(this) { ingredients ->
+            updateIngredientsGrid(ingredients)
+        }
     }
 
     private fun showImage(url: String) {
@@ -51,5 +55,35 @@ class RecipeInformationActivity : AppCompatActivity() {
             .load(url)
             .resize(4000, 2000)
             .into(binding.recipeImage)
+    }
+
+    private fun updateIngredientsGrid(ingredients: List<String>) {
+        val gridLayout = binding.gridLayoutIngredients
+        gridLayout.removeAllViews()
+
+        val columnCount = 2 // Define el número de columnas
+        val rowCount = (ingredients.size + columnCount - 1) / columnCount
+
+        for (i in 0 until rowCount) {
+            for (j in 0 until columnCount) {
+                val index = i * columnCount + j
+                if (index < ingredients.size) {
+                    val ingredient = ingredients[index]
+                    val textView = TextView(this)
+                    textView.text = "- $ingredient" // Agregar el prefijo numérico
+                    textView.setTextColor(getColor(R.color.white))
+                    textView.textSize = 20f
+                    textView.setPadding(10, 10, 10, 10)
+
+                    val params = GridLayout.LayoutParams()
+                    params.rowSpec = GridLayout.spec(i)
+                    params.columnSpec = GridLayout.spec(j)
+                    params.setMargins(10, 10, 10, 10)
+
+                    textView.layoutParams = params
+                    gridLayout.addView(textView)
+                }
+            }
+        }
     }
 }
